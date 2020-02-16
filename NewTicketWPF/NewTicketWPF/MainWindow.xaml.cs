dc96064fd4230814334fe1b0eeaec828368d19b0
Kawasakiy19 <61020952+Kawasakiy19@users.noreply.github.com>
2020-02-16 23:18:34 +0200

    asd

--- NewTicketWPF/NewTicketWPF/MainWindow.xaml.cs
+++ NewTicketWPF/NewTicketWPF/MainWindow.xaml.cs
@@ -21,6 +21,7 @@ namespace NewTicketWPF
     public partial class MainWindow : Window
     {
         Settings objSettings;
+        CreateProfileWindow objNewProfileWindow;
         Profile currentProfile;
         PData pData;
         public MainWindow()
@@ -46,6 +47,10 @@ namespace NewTicketWPF
             if (objSettings != null)
             {
                 objSettings.Close();
+            }
+            if (objNewProfileWindow != null)
+            {
+                objNewProfileWindow.Close();
             }
             Close();
         }
@@ -75,6 +80,26 @@ namespace NewTicketWPF
         {
             this.pData = pData;
             currentProfile = currentProfile.LoadSelectedProfile(pData);
+            TList.Items.Refresh();
+        }
+
+        public void NewProfileClick(object sender, RoutedEventArgs e)
+        {
+            objNewProfileWindow = new CreateProfileWindow();
+            objNewProfileWindow.SetCreatingForm = this;
+            IsEnabled = false;
+            objNewProfileWindow.Show();
+        }
+
+        public void RegenerateTicketClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void NewProfileChangesSet(Profile profile)
+        {
+            currentProfile = profile;
+            TList.ItemsSource = profile.SetProfile.tickets;
             TList.Items.Refresh();
         }
 
